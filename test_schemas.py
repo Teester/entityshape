e83aa576ba08d721d4dca102f524b1def8a7f8b2
Mark Tully <markjtully@gmail.com>
2021-04-03 00:32:56 +0100

    Adding test to check an entityschema with multiple statements about the same property

--- test_schemas.py
+++ test_schemas.py
@@ -98,5 +98,21 @@ class MyTestCase(unittest.TestCase):
             with self.subTest(prop=prop):
                 self.assertTrue(response.json["properties"][prop]["response"] in ["correct", "present"])
 
+    def test_entityschema_e297(self):
+        """
+        This test tests entityschema E297 (sailboat class) against entity
+        Q97179551 (J/92s).  The schema has multiple statements about the same property.
+        The test checks to ensure that the correct cardinality is calculated for
+        the relevant property
+        """
+        response = self.app.get(f'/api?entityschema=E297&entity=Q97179551&language=en',
+                                follow_redirects=True)
+        self.assertEqual(200, response.status_code)
+        properties: list = ["P2043", "P2048", "P2067"]
+        for prop in properties:
+            with self.subTest(prop=prop):
+                self.assertTrue(response.json["properties"][prop]["response"] in ["correct", "present"])
+
+
 if __name__ == '__main__':
     unittest.main()
