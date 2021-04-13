f8c4a7a0888c8ad0abd2914dada1c95f4f4b0e6d
Mark Tully <markjtully@gmail.com>
2021-04-14 00:19:45 +0100

    Fix sonarcloud and pylint errors

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+"""
+A Flask app to compare entityschema with wikidata items without using SPARQL
+"""
+from json import JSONDecodeError
+
 import requests
 
-from compareshape import CompareShape
 from flask import Flask, request, json
 from flask_cors import CORS
 from requests import Response
+from compareshape import CompareShape
 from shape import Shape
 
 app = Flask(__name__)
@@ -12,36 +17,58 @@ CORS(app)
 
 @app.route("/api")
 def data():
+    """
+    Compares an entityschema with a wikidata item
+    :return: a response to the query
+    """
     schema: str = request.args.get("entityschema", type=str)
     entity: str = request.args.get("entity", type=str)
     if "Lexeme" in entity:
         entity = entity[7:]
     language: str = request.args.get("language", type=str)
     try:
-        # valid: Dict = check_against_pyshexy(schema, entity)
+        # valid: dict = check_against_pyshexy(schema, entity)
         valid: dict = {}
         shape: Shape = Shape(schema, language)
-        comparison: CompareShape = CompareShape(shape.schema_shape, entity, language)
-        payload: dict = {'schema': schema, 'name': shape.name, 'validity': valid, 'general': comparison.general,
-                         'properties': comparison.properties, 'statements': comparison.statements, 'error': ""}
+        comparison: CompareShape = CompareShape(shape.get_schema_shape(), entity, language)
+        payload: dict = {'schema': schema,
+                         'name': shape.get_name(),
+                         'validity': valid,
+                         'general': comparison.get_general(),
+                         'properties': comparison.get_properties(),
+                         'statements': comparison.get_statements(),
+                         'error': ""}
         status: int = 200
-    except Exception as e:
-        payload = {'schema': "", 'name': "", 'validity': "", 'general': "", 'properties': "", 'statements': "",
-                   'error': "An error has occurred while translating this schema"}
+    except (AttributeError, TypeError, KeyError, IndexError) as exception:
+        payload: dict = {'schema': "",
+                         'name': "",
+                         'validity': "",
+                         'general': "",
+                         'properties': "",
+                         'statements': "",
+                         'error': "An error has occurred while translating this schema"}
         status = 500
-        print(f"Schema: {schema} - Error: {e}")
-    response: Response = app.response_class(response=json.dumps(payload), status=status, mimetype="application/json")
+        print(f"Schema: {schema} - {type(exception).__name__}: {exception}")
+    response: Response = app.response_class(response=json.dumps(payload),
+                                            status=status,
+                                            mimetype="application/json")
     return response
 
 
 def check_against_pyshexy(entityschema: str, entity: str):
+    """
+    Checks the entityschema and item against the pyshexy api
+    :param entityschema: the entityschema E number to be checked
+    :param entity: The entity Q number to be checked
+    :return: the response from pyshexy
+    """
     json_text: dict
     url: str = f"https://tools.wmflabs.org/pyshexy/api?entityschema={entityschema}&entity={entity}"
     try:
         response: Response = requests.get(url)
         json_text = response.json()
-    except Exception as e:
-        print(e)
+    except JSONDecodeError as exception:
+        print(f"{type(exception).__name__}: {exception}")
         json_text = {}
     return json_text
 
