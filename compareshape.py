b6db1851c6a9a3139fdbc8f22e89375d260e907e
Mark Tully <markjtully@gmail.com>
2021-04-13 01:52:48 +0100

    Refactor compareshape.py to make methods less complex

--- compareshape.py
+++ compareshape.py
@@ -1,3 +1,6 @@
+"""
+Compares a json shape from shape.py with wikidata json
+"""
 import requests
 
 from requests import Response
@@ -5,8 +8,8 @@ from requests import Response
 
 class CompareShape:
     """
-    Compares a wikidata entity (e.g. Q42) with a shape and returns the conformity of the statements and properties in
-    the entity to the shape
+    Compares a wikidata entity (e.g. Q42) with a shape and returns the conformity of
+    the statements and properties in the entity to the shape
 
     :param shape: The a json representation of the shape to be compared against
     :param entity: The entity to be compared (e.g. Q42)
@@ -19,9 +22,9 @@ class CompareShape:
     # TODO: Process OR and NOT comparison
     # TODO: Process groups in comparison
     def __init__(self, shape: dict, entity: str, language: str):
-        self.properties: dict = {}
-        self.statements: dict = {}
-        self.general: dict = {}
+        self._properties: dict = {}
+        self._statements: dict = {}
+        self._general: dict = {}
 
         self._entity: str = entity
         self._language: str = language
@@ -36,15 +39,36 @@ class CompareShape:
         self._compare_statements()
         self._compare_properties()
 
+    def get_properties(self) -> dict:
+        """
+        Gets the result of comparison for each property with the schema
+        :return: json for comparison of properties
+        """
+        return self._properties
+
+    def get_statements(self) -> dict:
+        """
+        Gets the result of comparison of each statement with the schema
+        :return: json for comparison of statements
+        """
+        return self._statements
+
+    def get_general(self) -> dict:
+        """
+        Gets general properties of the comparison
+        :return: json for general properties of the comparison
+        """
+        return self._general
+
     def _get_general(self):
         properties: list = ["lexicalCategory", "language"]
         for item in properties:
             if item in self._shape and item in self._entities["entities"][self._entity]:
                 expected: list = self._shape[item]["allowed"]
                 actual: str = self._entities["entities"][self._entity][item]
-                self.general[item] = "incorrect"
+                self._general[item] = "incorrect"
                 if actual in expected:
-                    self.general[item] = "correct"
+                    self._general[item] = "correct"
 
     def _compare_statements(self):
         """
@@ -57,77 +81,13 @@ class CompareShape:
             for statement in claims[claim]:
                 child: dict = {"property": claim}
                 allowed: str = "not in schema"
-                qualifiers: str = ""
-                required: str = ""
-                extra: str = ""
                 if claim in self._shape:
-                    allowed = "present"
-                    if "necessity" in self._shape[claim]:
-                        child["necessity"] = self._shape[claim]["necessity"]
-                        allowed = "allowed"
-                    if "allowed" in self._shape[claim]:
-                        allowed = "correct"
-                        if "id" in statement["mainsnak"]["datavalue"]["value"]:
-                            value: str = statement["mainsnak"]["datavalue"]["value"]["id"]
-                            if value not in self._shape[claim]["allowed"]:
-                                allowed = "incorrect"
-                    if "not_allowed" in self._shape[claim] and \
-                            "id" in statement["mainsnak"]["datavalue"]["value"]:
-                        value: str = statement["mainsnak"]["datavalue"]["value"]["id"]
-                        if value in self._shape[claim]["not_allowed"]:
-                            allowed = "not allowed"
-                    if "extra" in self._shape[claim]:
-                        extra: str = "extra"
-                    if "qualifiers" in self._shape[claim]:
-                        allowed_qualifiers: list = []
-                        for qualifier in self._shape[claim]["qualifiers"]:
-                            if "qualifiers" in statement and qualifier not in statement["qualifiers"]:
-                                allowed_qualifiers.append(qualifier)
-                        if len(allowed_qualifiers) > 0:
-                            qualifiers: str = "missing qualifiers: " + ", ".join(allowed_qualifiers)
-                        else:
-                            qualifiers = ""
-                    if "required" in self._shape[claim]:
-                        if "required" in self._shape[claim]["required"]:
-                            required_property: str = list(self._shape[claim]["required"]["required"].keys())[0]
-                            required_value: str = self._shape[claim]["required"]["required"][required_property][0]
-                        else:
-                            required_property: str = list(self._shape[claim]["required"].keys())[0]
-                            required_value: str = self._shape[claim]["required"][required_property][0]
-
-                        query_entity: str = statement["mainsnak"]["datavalue"]["value"]["id"]
-                        url: str = f"https://www.wikidata.org/w/api.php?action=wbgetclaims" \
-                            f"&entity={query_entity}&property={required_property}&format=json"
-                        response: Response = requests.get(url)
-                        json_text: dict = response.json()
-                        if required_property in json_text["claims"]:
-                            for key in json_text["claims"][required_property]:
-                                if key["mainsnak"]["datavalue"]["value"]["id"] == required_value:
-                                    required = "present"
-                                else:
-                                    required = "incorrect"
-                        else:
-                            required = "missing"
-                if required == "present":
-                    if qualifiers == "":
-                        allowed = "correct"
-                    else:
-                        allowed = qualifiers
-                if required == "incorrect":
-                    if extra == "extra":
-                        allowed = "allowed"
-                    else:
-                        if qualifiers == "":
-                            allowed = "allowed"
-                        else:
-                            allowed = qualifiers
-                if allowed == "incorrect" and extra == "extra":
-                    allowed = "allowed"
-                if required == "missing":
-                    allowed = required
+                    allowed, extra, qualifiers, required = \
+                        self._process_claim_in_shape(claim, statement, child)
+                    allowed = self._process_allowed(allowed, required, qualifiers, extra)
                 if allowed != "":
                     child["response"] = allowed
-                self.statements[statement["id"]] = child
+                self._statements[statement["id"]] = child
                 statement_results.append(allowed)
                 if allowed.startswith("missing"):
                     allowed = "incorrect"
@@ -147,7 +107,7 @@ class CompareShape:
                 response = self._process_claim(claim, child)
             if response != "":
                 child["response"] = response
-            self.properties[claim] = child
+            self._properties[claim] = child
 
     def _process_claim(self, claim, child):
         cardinality: str = ""
@@ -159,7 +119,7 @@ class CompareShape:
         else:
             allowed = "present"
         if claim in self._shape:
-            cardinality, max_cardinality, min_cardinality = self._assess_cardinality(claim, child)
+            cardinality = self._assess_cardinality(claim, child)
         if cardinality == "correct":
             response = allowed
         else:
@@ -170,27 +130,24 @@ class CompareShape:
 
     def _assess_cardinality(self, claim, child):
         cardinality: str = ""
-        max_cardinality: bool = False
-        min_cardinality: bool = False
         if child["necessity"] != "absent":
             cardinality = "correct"
         if "cardinality" in self._shape[claim]:
+            claim_cardinality = self._shape[claim]["cardinality"]
             number_of_statements: int = len(self._property_responses[claim])
             if "extra" in self._shape[claim]:
                 number_of_statements = self._property_responses[claim].count("correct")
             min_cardinality = True
             max_cardinality = True
-            if "min" in self._shape[claim]["cardinality"] and \
-                    number_of_statements < self._shape[claim]["cardinality"]["min"]:
+            if "min" in claim_cardinality and number_of_statements < claim_cardinality["min"]:
                 min_cardinality = False
-            if "max" in self._shape[claim]["cardinality"] and \
-                    number_of_statements > self._shape[claim]["cardinality"]["max"]:
+            if "max" in claim_cardinality and number_of_statements > claim_cardinality["max"]:
                 max_cardinality = False
             if min_cardinality and not max_cardinality:
                 cardinality = "too many statements"
             if max_cardinality and not min_cardinality:
                 cardinality = "not enough correct statements"
-        return cardinality, max_cardinality, min_cardinality
+        return cardinality
 
     def _get_entity_json(self):
         """
@@ -222,8 +179,8 @@ class CompareShape:
                                         for i in range((len(self._props) + 48) // 48)]
         for element in wikidata_property_list:
             required_properties: str = "|".join(element)
-            url: str = f"https://www.wikidata.org/w/api.php?action=wbgetentities&ids={required_properties}&" \
-                       f"props=labels&languages={self._language}&format=json"
+            url: str = f"https://www.wikidata.org/w/api.php?action=wbgetentities&ids=" \
+                       f"{required_properties}&props=labels&languages={self._language}&format=json"
             response: Response = requests.get(url)
             json_text: dict = response.json()
             for item in element:
@@ -232,3 +189,96 @@ class CompareShape:
                         json_text["entities"][item]["labels"][self._language]["value"]
                 except KeyError:
                     self._names[json_text["entities"][item]["id"]] = ""
+
+    def _process_allowed_in_shape_claim(self, claim, param):
+        allowed = "correct"
+        if "id" in param["value"]:
+            value: str = param["value"]["id"]
+            if value not in self._shape[claim]["allowed"]:
+                allowed = "incorrect"
+        return allowed
+
+    @staticmethod
+    def _process_required_in_shape_claim(shape_claim, datavalue):
+        required: str = ""
+        if "required" in shape_claim["required"]:
+            shape_claim_required = shape_claim["required"]["required"]
+            required_property: str = list(shape_claim_required.keys())[0]
+            required_value: str = shape_claim_required[required_property][0]
+        else:
+            required_property: str = list(shape_claim["required"].keys())[0]
+            required_value: str = shape_claim["required"][required_property][0]
+
+        query_entity: str = datavalue["value"]["id"]
+        url: str = f"https://www.wikidata.org/w/api.php?action=wbgetclaims" \
+                   f"&entity={query_entity}&property={required_property}&format=json"
+        response: Response = requests.get(url)
+        json_text: dict = response.json()
+        if required_property in json_text["claims"]:
+            for key in json_text["claims"][required_property]:
+                if key["mainsnak"]["datavalue"]["value"]["id"] == required_value:
+                    required = "present"
+                else:
+                    required = "incorrect"
+        else:
+            required = "missing"
+        return required
+
+    @staticmethod
+    def _process_allowed(allowed, required, qualifiers, extra):
+        if required == "present":
+            if qualifiers == "":
+                allowed = "correct"
+            else:
+                allowed = qualifiers
+        if required == "incorrect":
+            if extra == "extra":
+                allowed = "allowed"
+            else:
+                if qualifiers == "":
+                    allowed = "allowed"
+                else:
+                    allowed = qualifiers
+        if allowed == "incorrect" and extra == "extra":
+            allowed = "allowed"
+        if required == "missing":
+            allowed = required
+        return allowed
+
+    @staticmethod
+    def _process_qualifiers_in_shape_claim(shape_claim, statement):
+        allowed_qualifiers: list = []
+        for qualifier in shape_claim["qualifiers"]:
+            if "qualifiers" in statement and \
+                    qualifier not in statement["qualifiers"]:
+                allowed_qualifiers.append(qualifier)
+        if len(allowed_qualifiers) > 0:
+            qualifiers: str = "missing qualifiers: " + ", ".join(allowed_qualifiers)
+        else:
+            qualifiers = ""
+        return qualifiers
+
+    def _process_claim_in_shape(self, claim, statement, child):
+        datavalue: dict = statement["mainsnak"]["datavalue"]
+        shape_claim: dict = self._shape[claim]
+        qualifiers: str = ""
+        required: str = ""
+        extra: str = ""
+
+        allowed = "present"
+        if "necessity" in shape_claim:
+            child["necessity"] = shape_claim["necessity"]
+            allowed = "allowed"
+        if "allowed" in shape_claim:
+            allowed = self._process_allowed_in_shape_claim(claim, datavalue)
+        if "not_allowed" in shape_claim and "id" in datavalue["value"]:
+            value: str = datavalue["value"]["id"]
+            if value in shape_claim["not_allowed"]:
+                allowed = "not allowed"
+        if "extra" in shape_claim:
+            extra: str = "extra"
+        if "qualifiers" in shape_claim:
+            qualifiers = self._process_qualifiers_in_shape_claim(shape_claim, statement)
+        if "required" in shape_claim:
+            required = self._process_required_in_shape_claim(shape_claim, datavalue)
+        return allowed, extra, qualifiers, required
