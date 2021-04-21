aef896c2feea77b556582fb7cd45840a351e5248
Mark Tully <markjtully@gmail.com>
2021-04-22 00:23:04 +0100

    Update script.js to match script on wikidata

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+(function() {
 let entitycheck_stylesheet = entitycheck_getStylesheet();
 $('html > head').append("<style>" + entitycheck_stylesheet + "</style>");
 
@@ -22,8 +23,8 @@ let entitycheck_conditions = ["/wiki/Q", "/wiki/P", "/wiki/L"];
 if (entitycheck_conditions.some(el => document.location.pathname.includes(el))) {
 	let entitycheck_entity_html = '<div><span id="entityschema-simpleSearch"><span>';
 	entitycheck_entity_html += '<input type="text" id="entityschema-entityToCheck" placeholder="Enter a schema to check against e.g. E234">';
-	entitycheck_entity_html += '<input type="submit" id="entityschema-schemaSearchButton" class="searchButton" onClick="entitycheck_update()" name="check" value="Check">';
-	entitycheck_entity_html += '</span></span><input type="checkbox" id="entitycheck-checkbox" onclick="entitycheck_checkbox()">';
+	entitycheck_entity_html += '<input type="submit" id="entityschema-schemaSearchButton" class="searchButton" name="check" value="Check">';
+	entitycheck_entity_html += '</span></span><input type="checkbox" id="entitycheck-checkbox">';
 	entitycheck_entity_html += '<label for="entitycheck-checkbox"><small>Automatically check schema</small></label><span id="entityCheckResponse"></span></div>';
 	var entitycheck_entityTitle = $(".wikibase-title-id" )[0].innerText;
 	if (document.location.pathname.includes("/wiki/L")) {
@@ -31,6 +32,8 @@ if (entitycheck_conditions.some(el => document.location.pathname.includes(el))) 
 	} else {
 		$(".wikibase-entitytermsview-heading" ).append( entitycheck_entity_html );
 	}
+	$("#entityschema-schemaSearchButton").click(function(){ entitycheck_update() });
+	$("#entitycheck-checkbox").click(function() { entitycheck_checkbox() })
 }
 
 $("#entityschema-entityToCheck").on("keyup", function(event){
@@ -61,13 +64,11 @@ function entitycheck_checkEntity(entity, entitySchema, language) {
 	$("#entityCheckResponse").contents().remove();
 	$(".entitycheck-property").remove();
 	let url = "https://entityshape.toolforge.org/api?entityschema=" + entitySchema + "&entity=" + entity + "&language=" + language;
-	// let url = "https://127.0.0.1:5000/api?entityschema=" + entitySchema + "&entity=" + entity + "&language=" + language;
 	$.ajax({
 		type: "GET",
 		dataType: "json",
 		url: url,
 		success: function(data){
-		console.log(data);
 			let html = "";
 
 			html += "<br/>Checking against <b><a href='https://www.wikidata.org/wiki/EntitySchema:" + data.schema + "'>" + data.schema + ":" + data.name + '</a></b>:';
@@ -78,7 +79,7 @@ function entitycheck_checkEntity(entity, entitySchema, language) {
 					html += entitycheck_parseResult(data.validity.results[0]);
 				}
 			}
-			html += '<div style="overflow-y: scroll; max-height:200px;"><table style="width:100%;">'
+			html += '<div style="overflow-y: scroll; max-height:200px;"><table style="width:100%;">';
 			html += '<th class="entitycheck_table" title="Properties in this item which must be present">Required properties</th>';
 			html += '<th class="entitycheck_table" title="Properties in this item which can be present but do not have to be">Optional properties</th>';
 			html += '<th class="entitycheck_table" title="Properties in this item which are not allowed to be present or not mentioned in the entityschema">Other properties</th><tr>';
@@ -91,7 +92,6 @@ function entitycheck_checkEntity(entity, entitySchema, language) {
 					let shape_html = "";
 					let response1 = data.properties[key].response;
 					let response_class  = "";
-					console.log(data.properties[key])
 					switch (response1) {
 						case "present":
 							response_class = "present";
@@ -234,3 +234,4 @@ function entitycheck_getStylesheet() {
 	stylesheet += ".entitycheck-span { color: #ffffff; padding:2px; margin: 2px; font-size:75%; border-radius:2px; }";
 	return stylesheet;
 }
+}());
