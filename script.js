78d4b372fbac5eb4bc9a313138d9406b85809a52
Mark Tully <markjtully@gmail.com>
2024-07-04 00:37:10 +0100

    Handle when there is no schema entered or automatically determined one in the UI

--- script.js
+++ script.js
@@ -97,10 +97,14 @@ function entityschema_update() {
     	window.localStorage.setItem("entityschema", "");
 	} else {
     	window.localStorage.setItem("entityschema", entityschema_entitySchema);
+    }
+	if (entityschema_entitySchema.length == 0) {
+	    $("#entityschemaResponse").append( '<span>No schemas entered and could not automatically determine schemas to check</span>' );
+	} else {
+	    let entityschema_entityName = document.location.pathname.substring(6);
+	    let lang = mw.config.get( 'wgUserLanguage' );
+	    entityschema_checkEntity(entityschema_entityName, entityschema_entitySchema, lang);
 	}
-	let entityschema_entityName = document.location.pathname.substring(6);
-	let lang = mw.config.get( 'wgUserLanguage' );
-	entityschema_checkEntity(entityschema_entityName, entityschema_entitySchema, lang);
 }
 
 function entityschema_checkbox() {
