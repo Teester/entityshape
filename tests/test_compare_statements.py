ffcc26fa99a6fbd8755a07d4efb36067c6e69578
Mark Tully <markjtully@gmail.com>
2025-04-24 22:19:14 +0100

    Adding tests to test CompareStatements()

---
+++ tests/test_compare_statements.py
@@ -0,0 +1,106 @@
+import unittest
+
+from comparejsonld import CompareStatements
+
+
+class TestCompareStatements(unittest.TestCase):
+    def setUp(self):
+        entities = {'entities':
+                        {'Q1':
+                             {'title': 'Q1',
+                              'type': 'item',
+                              'id': 'Q1',
+                              'claims': {'P31': [{'mainsnak':
+                                                      {'snaktype': 'value',
+                                                       'property': 'P31',
+                                                       'hash': '1',
+                                                       'datavalue': {'value':
+                                                                         {'entity-type': 'item', 'numeric-id': 2, 'id': 'Q2'},
+                                                                     'type': 'wikibase-entityid'},
+                                                       'datatype': 'wikibase-item'},
+                                                  'type': 'statement',
+                                                  'id': '1',
+                                                  'rank': 'normal'}],
+                                         },
+                              'sitelinks': {}}}}
+        entity = "Q1"
+        statement = {  "type": "Shape",
+                       "id": "test",
+                       "expression": {
+                           "type": "EachOf",
+                           "expressions": [
+                               {
+                                   "type": "TripleConstraint",
+                                   "predicate": "http://www.wikidata.org/prop/direct/P31"
+                               }
+                           ]
+                       }
+                       }
+        self.compare_statements =  CompareStatements(entities, entity, statement)
+
+    def test_compare_statements_with_nothing(self):
+        entities = {}
+        entity = ""
+        statement = {}
+        test_method =  CompareStatements(entities, entity, statement)
+        self.assertEqual({}, test_method.compare_statements())
+
+    def test_compare_statements(self):
+        result = {'1': {'necessity': 'required',
+                        'property': 'P31',
+                        'response': 'allowed'}}
+        self.assertEqual(result, self.compare_statements.compare_statements())
+
+    def test_process_shape_with_nothing(self):
+        statement = {}
+        self.assertEqual("", self.compare_statements._process_shape(statement))
+
+    def test_process_shape_with_values(self):
+        statement = {'snaktype': 'value',
+                     'property': 'P31',
+                     'hash': '1',
+                     'datavalue': {'value':
+                                       {'entity-type': 'item', 'numeric-id': 2, 'id': 'Q2'},
+                                   'type': 'wikibase-entityid'},
+                     'datatype': 'wikibase-item'}
+        self.assertEqual("allowed", self.compare_statements._process_shape(statement))
+
+    def test_process_expressions_with_nothing(self):
+        expression = {}
+        statement = {}
+        self.assertEqual("", self.compare_statements.process_expressions(expression, statement))
+
+    def test_process_expressions_with_values(self):
+        expression = {"type": "TripleConstraint",
+                      "predicate": "http://www.wikidata.org/prop/direct/P31"
+                      }
+        statement = {'snaktype': 'value',
+                     'property': 'P31',
+                     'hash': '1',
+                     'datavalue': {'value':
+                                       {'entity-type': 'item', 'numeric-id': 2, 'id': 'Q2'},
+                                   'type': 'wikibase-entityid'},
+                     'datatype': 'wikibase-item'}
+        self.assertEqual("allowed", self.compare_statements.process_expressions(expression, statement))
+
+    def test_process_triple_constraint_with_nothing(self):
+        statement = {}
+        expression = {}
+        self.assertEqual("", self.compare_statements._process_triple_constraint(statement, expression))
+
+    def test_process_triple_constraint_with_values(self):
+        expression = {"type": "TripleConstraint",
+                      "predicate": "http://www.wikidata.org/prop/direct/P31"
+                      }
+        statement = {'snaktype': 'value',
+                     'property': 'P31',
+                     'hash': '1',
+                     'datavalue': {'value':
+                                       {'entity-type': 'item', 'numeric-id': 2, 'id': 'Q2'},
+                                   'type': 'wikibase-entityid'},
+                     'datatype': 'wikibase-item'}
+        self.assertEqual("allowed", self.compare_statements._process_triple_constraint(statement, expression))
+
+
+if __name__ == '__main__':
+    unittest.main()
