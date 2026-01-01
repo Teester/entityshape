01d66641406be67c8523159af7dc59b3f89baf96
Mark Tully <markjtully@gmail.com>
2026-01-01 16:57:22 +0000

    WIP

--- entityshape/api_v3/compareProperties2.py
+++ entityshape/api_v3/compareProperties2.py
@@ -1,14 +1,74 @@
 class CompareProperties2:
     def __init__(self, entity: str, entities: dict, props: list, schema: dict, names: dict) -> None:
-        self._entity = entity
-        self._entities = entities
-        self._props = props
-        self._schema = schema
-        self._names = names
-        self._start_shape = self._get_start_shape()
+        self._entity: str = entity
+        self._entities: dict = entities
+        self._props: list = props
+        self._schema: dict = schema
+        self._names: dict = names
+        self._start_shape: str = self._get_start_shape()
 
     def compare_properties(self) -> dict:
+        if self._start_shape == "":
+            return {}
+        if "entities" not in self._entities:
+            return {}
+        if self._entity not in self._entities["entities"]:
+            return {}
+
+        shape: dict = self._get_shape(self._start_shape)
+        entity: dict = self._entities["entities"][self._entity]
+        response: dict = self._process_shape(shape, entity)
+        return response
+
+    def _process_shape(self, shape: dict, entity: dict) -> dict:
+        # process the shape - is it a schema, a shape, a triple constraint, a node constraint etc
+        if "type" not in shape:
+            return {}
+        if shape["type"] == "Schema":
+            return self._process_schema(shape, entity)
+        if shape["type"] == "Shape":
+            return self._process_shape_in_shape(shape, entity)
+        if shape["type"] == "EachOf":
+            return self._process_each_of(shape, entity)
+        if shape["type"] == "OneOf":
+            return self._process_one_of()
+        if shape["type"] == "TripleConstraint":
+            return self._process_triple_constraint(shape, entity)
+        print("none of the above")
         return {}
+
+    def _process_schema(self, schema, entity) -> dict:
+        """
+        Processes a schema type
+
+        :param schema: the schema to be processed
+        :param entity: the entity to be processed
+        :return: a result
+        """
+        for shape in schema["shapes"]:
+            if shape["id"] == schema["start"]:
+                return self._process_shape(shape, entity)
+        return {}
+
+    def _process_shape_in_shape(self, shape, entity) -> dict:
+        if "expression" in shape:
+            return self._process_shape(shape["expression"], entity)
+        return {}
+
+    def _process_each_of(self, shape, entity) -> dict:
+        if shape["id"] == self._start_shape:
+            return self._process_each_of_for_start_shape(shape["expressions"], entity)
+        else:
+            return self._process_each_of_for_non_start_shape(shape["expressions"], entity)
+
+    def _process_one_of(self) -> dict:
+        return {}
+
+    def _process_triple_constraint(self, shape, entity) -> dict:
+        property_name: str = self._get_property_name(shape["predicate"])
+        return {"name": property_name,
+                "necessity": self._get_necessity(shape),
+                "response": self._process_triple_constraint_2(shape, entity, "")}
 
     def _get_start_shape(self) -> str:
         """
@@ -45,3 +105,12 @@ class CompareProperties2:
         :return: the property as a string
         """
         return predicate.rsplit('/', 1)[1]
+
+    @staticmethod
+    def _get_necessity(expression: dict) -> str:
+        necessity: str = "optional"
+        if ("min" in expression and expression["min"] > 0) or ("min" not in expression and "max" not in expression):
+            necessity = "required"
+        if "min" in expression and "max" in expression and expression["min"] == 0 and expression["max"] == 0:
+            necessity = "absent"
+        return necessity
